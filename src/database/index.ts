cec0eb0cfa96886b62608dcb145bc31f2cb27c07
maiconparra <maiconparradasilva@gmail.com>
2022-05-13 18:18:15 -0300

    Merge development to main

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,14 +2,12 @@ import { Sequelize } from 'sequelize';
 
 import 'dotenv/config';
 
-console.log('username: ', `${process.env.DIALECT}://${process.env.USERDATABASE}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`);
-
 //Mappings
 import User from './mappings/user.mapping';
 import Endereco from './mappings/endereco.mapping';
 import Contatos from './mappings/contato.mapping';
 
-const connection = new Sequelize(`${process.env.DIALECT}://${process.env.USERDATABASE}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`);
+const connection = new Sequelize(`${process.env.DIALECT}://${process.env.USERDATABASE}:${process.env.PASSWORD}@${process.env.HOST}:25060/${process.env.DATABASE}`);
 
 User.onLoadUser(connection);
 Endereco.onLoadEndereco(connection);
